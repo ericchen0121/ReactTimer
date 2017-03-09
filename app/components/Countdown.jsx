494d3ee650dc7d8ed5b3fe9af4b939d8e5e5de10
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-08 22:35:19 -0800

    clear timer interval when countdown component is unmounted

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -29,13 +29,16 @@ var Countdown = React.createClass({
       }
     }
   },
-
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function() {
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
-      this.setState({
-        count: (newCount >= 0) ? newCount : 0 // don't set count to a negative!
-      })
+      
+      // don't set count to a negative!
+      this.setState({count: (newCount >= 0) ? newCount : 0 })
 
       if (newCount === 0) {
         this.setState({countdownStatus: 'stopped'})
